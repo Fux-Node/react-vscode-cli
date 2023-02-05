06b557bd7c225b81d4384859e3881dfefefca88d
sailendra dash <75584628+narkreeta@users.noreply.github.com>
2023-02-05 11:23:06 +0000

    folder path create

--- cli/index.js
+++ cli/index.js
@@ -1,13 +1,19 @@
 #!/usr/bin/env node
+import chalk from 'chalk';
+import {exec} from "child_process"
 let args = process.argv.slice(2);
 
+
 const commandAction = (params) => {
-    return new Promise((res,rej)=>{
-        setTimeout(()=>{rej("success")},7000)
-    })
+    const name = params[0]
+    const re = new RegExp("^[a-zA-Z0-9]+$", "g")
+    if (re.test(name)) {
+        exec(`mkdir ${name}`,(err,stdout,stderr)=>{
+            console.log({err,stdout,stderr});
+        });
+    } else {
+        console.log(chalk.red('Entered Name is not Valid. Please put valid name.'));
+    }
 }
 
-
-
-
-commandAction(args).then(()=>clearInterval(startPrint)).catch((err)=>console.warn("error"))
+commandAction(args)
